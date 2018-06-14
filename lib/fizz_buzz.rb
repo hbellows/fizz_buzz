1129e97e2c6886d7134ec4cda00e6867ec47843e
Harper Bellows <harper.bellows@gmail.com>
2018-06-14 15:45:56 -0600

    rearrange program to have runner class called fizz_buzz and controller class called game_controller

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,47 +1,17 @@
 class FizzBuzz
 
-  def initialize
-    @numbers = (1..25)
-  end
+  # def game
+  #   1000.times do |num|
+  #   fizz_buzz = FizzBuzz.new(num)
+  #   fizz_buzz.start
+  # end
+
+
 
   def super_fizz_buzz(numbers)
-    numbers = @numbers
+    @numbers = (1..25)
     fizz = fizz(numbers)
     buzz = buzz(fizz)
   #   super_ = super_(buzz)
   end
-
-  def fizz(numbers)
-    fizz = numbers.map do |num|
-      if num % 3 == 0
-        'Fizz'
-      else
-        num
-      end
-    end
-    fizz
-  end
-
-  def buzz(fizz)
-    buzz = fizz.map do |num|
-      if num % 5 == 0
-        'Buzz'
-      else
-        num
-      end
-    end
-    buzz
-  end
-
-  def super_(buzz)
-    super_ = buzz.map do |num|
-      if num % 7 == 0 
-        'Super'
-      else
-        num
-      end
-    end
-    super_
-  end
-
 end
