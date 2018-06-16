4b2af078938009ed208af61f91d422dc3051ba84
Harper Bellows <harper.bellows@gmail.com>
2018-06-16 10:57:30 -0600

    refactor tests for new logic model

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -1,13 +1,15 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/fizz_buzz.rb'
+require './lib/game_controller.rb'
 
 class FizzBuzzTest < Minitest::Test
 
   def test_it_can_play_Super_Fizz_Buzz
-    game = GameController.new(1..8)
+    input = (1..8)
+    fizz_buzz = FizzBuzz.new(input)
 
     result = 1, 2, 'Fizz', 3, 4, 'Buzz', 'Fizz', 'Super', 8
-    assert_equal result, game.super_fizz_buzz
+    assert_equal result, fizz_buzz.play_game(input)
   end
 end
