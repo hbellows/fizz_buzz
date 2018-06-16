c3a1c08fb2e5376d2ee9e335a7614b36104af91b
Harper Bellows <harper.bellows@gmail.com>
2018-06-16 10:57:07 -0600

    rebuild logic model

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -1,47 +1,92 @@
 class GameController
 
   def initialize(input)
-    @numbers = input
-    #@numbers = (1..25)
+    @input = input
+    @output = ''
   end
 
-  def fizz(numbers)
-    numbers = @numbers
-    fizz = numbers.map do |num|
-      if num % 3 == 0
-        'Fizz'
-      else
-        num
-      end
-    end
-    # fizz
-  end
-
-  def buzz(fizz)
-    buzz = fizz.map do |num|
-      if num % 5 == 0
-        'Buzz'
-      else
-        num
-      end
-    end
-    # buzz
-  end
-
-  def super_(buzz)
-    super_ = buzz.map do |num|
+  def divisible_by_seven(input)
+    input.map do |num|
       if num % 7 == 0
-        'Super'
-      else
-        num
+        @output << 'Super'
       end
     end
-    # super_
   end
 
-  def fizz_buzz
-    # divisble by 3 and 5, first instance is 15
-
+  def divisible_by_five(input)
+    input.map do |num|
+      if num % 5 == 0
+        @output << 'Buzz'
+      end
+    end
   end
 
+  def divisble_by_three(input)
+    input.map do |num|
+      if num % 3 == 0
+        @output << 'Fizz'
+      end
+    end
+  end
 end
+
+
+  # def find_fizz_buzz(supery)
+  #   # fizzy_buzzy =
+  #   supery.map do |num|
+  #     if num % 3 == 0 && num % 5 == 0
+  #       "FizzBuzz"
+  #     else
+  #       num
+  #     end
+  #   end
+  # end
+  #
+  # def find_super_buzz(fizzy_buzzy)
+  #   # super_buzzy =
+  #   fizzy_buzzy.map do |num|
+  #     if num % 5 == 0 && num % 7 == 0
+  #       "SuperBuzz"
+  #     else
+  #       num
+  #     end
+  #   end
+  # end
+  #
+  # def find_super_fizz(super_buzzy)
+  #   # super_fizzy =
+  #   super_buzzy.map do |num|
+  #     if num % 3 == 0 && num % 7 == 0
+  #       'SuperFizz'
+  #     else
+  #       num
+  #     end
+  #   end
+  # end
+  #
+  # def find_super_fizz_buzz(super_fizzy)
+  #   # super_fizzy_buzzy =
+  #   super_fizzy.map do |num|
+  #     if num % 3 == 0 && num % 5 == 0 && num % 7 == 0
+  #       'SuperFizzBuzz'
+  #     else
+  #       num
+  #     end
+  #   end
+  # end
+
+
+# A number has a corresponding output. The rules for determining the output are as follows:
+#
+# If it’s evenly divisible by 3, 5, and 7 the output is SuperFizzBuzz
+#   first instance of SuperFizzBuzz is 105
+# If it’s evenly divisible by 3 and 7 the output is SuperFizz
+#   first instance of SuperFizz is 21
+# If it’s evenly divisible by 5 and 7 the output is SuperBuzz
+#   fist instance of SuperBuzz is 35
+# If it’s evenly divisible by 3 and 5 the output is FizzBuzz
+#   first instance of FizzBuzz is 15
+# If it’s evenly divisible by 3, the output is Fizz
+# If it’s evenly divisible by 5, the output is Buzz
+# If it’s evenly divisible by 7, the output is Super
+# Otherwise the output is just the number
