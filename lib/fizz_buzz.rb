ab528c23962c8f3100f3c938fa4467677e3a3bf3
Harper Bellows <harper.bellows@gmail.com>
2018-06-20 17:00:45 -0600

    final commit

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,8 +1,13 @@
 require './lib/game_controller'
 
-
+  puts "Please play my game!"
+  puts "The game accepts either a single number, or a range of numbers."
+  puts "To enter a single number, simply enter the number."
+  puts "To enter a range of numbers, enter the first and last numbers of the range, separated by a space (the range of 1-25, would be 1 25)."
+  puts "\n"
   puts "Please enter a number, or a range of numbers:"
   print '> '
+
   user_input = gets.chomp
   input = user_input.split.map {|num| num.to_i}
   game = GameController.new(input)
